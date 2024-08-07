5054449e1de7ca103c8511aaf4bdc576f238a279
theskyblockman <theskyblockman@gmail.com>
2024-08-07 22:03:47 +0200

    version change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "fr.theskyblockman.life_chest"
         minSdk = 26
         targetSdk = 34
-        versionCode = 20000
-        versionName = "2.0.0"
+        versionCode = 20001
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
